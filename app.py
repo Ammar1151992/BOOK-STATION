f08fedaa1932cb5ae82c3ffdafa69b3349b58605
Ammar1151992 <ammar.alani.it@gmail.com>
2022-03-18 15:47:58 +0300

    update the files

---
+++ app.py
@@ -0,0 +1,270 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_wtf import FlaskForm
+from flask_login import current_user, LoginManager
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import UserMixin, login_user, logout_user
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8699cbd6a17b478370a6f05d29ec1d25'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookdatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Ammarh1151992@localhost/db_books'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+migrate = Migrate(app, db)
+
+
+
+ 
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", title='Home')
+
+@app.before_first_request
+def create_db():
+    db.create_all()
+
+
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html", title='cart')
+
+
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_name = user.query.filter_by(email=form.email.data).first()
+        if user_name and bcrypt.check_password_hash(user_name.password, form.password.data):
+            login_user(user_name, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('يرجى التحقق من الايميل او الرقم السري', 'error')
+    return render_template('login.html', title='Login', form=form)
+     
+
+@app.route("/regist", methods=['GET','POST'])
+def regist():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user_name = user(firstname=form.firstname.data, lastname=form.lastname.data, email=form.email.data, password=hashed_password)
+        db.session.add(user_name)
+        db.session.commit()
+        flash(f'{form.firstname.data}  تم انشاء الحساب بنجاح، من فضلك قم بتسجيل حسابك الان' , 'success')
+        return redirect(url_for('login'))
+    return render_template('regist.html', title='Register', form=form)
+ 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/english_book")
+def english_book():
+    return render_template('english_book.html', title='english_book')
+
+
+
+
+@app.route("/selected/<string:type>", methods=['GET'])
+def selected(type):
+    if not current_user.is_authenticated:
+        return redirect(url_for('regist'))
+    type_book = book.query.filter_by(type=type).all()
+    if not type_book:
+        return redirect('/home')
+    # books = select(book).where(book.type=="?",type_book)
+    return render_template('selected.html', title='selected', books=type_book)
+
+
+@app.route("/vew_product/<int:id>", methods=['GET'])
+def vew_product(id):
+    if not current_user.is_authenticated:
+        return redirect(url_for('regist'))
+    id_book = book.query.filter_by(id=id).first()
+    if not id_book:
+        return redirect('/home')
+    # books = select(book).where(book.type=="?",type_book)
+    return render_template('vew_product.html', title='selected', book=id_book)
+
+
+
+
+@app.route("/get_free/<string:type>")
+def get_free(type):
+    if not current_user.is_authenticated:
+        return redirect(url_for('regist'))
+    type_book = book.query.filter_by(type=type).all()
+    if not type_book:
+        return redirect('/home')
+    # books = select(book).where(book.type=="?",type_book)
+    return render_template("get_free.html", title='get_free', books=type_book)
+
+
+@app.route("/last_released/<string:type>")
+def last_released(type):
+    if not current_user.is_authenticated:
+        return redirect(url_for('regist'))
+    type_book = book.query.filter_by(type=type).all()
+    if not type_book:
+        return redirect('/home')
+    # books = select(book).where(book.type=="?",type_book)
+    return render_template("last_released.html", title='last_released', books=type_book)
+
+
+
+@app.route("/free_delivery/<string:type>")
+def free_delivery(type):
+    if not current_user.is_authenticated:
+        return redirect(url_for('regist'))
+    type_book = book.query.filter_by(type=type).all()
+    if not type_book:
+        return redirect('/home')
+    # books = select(book).where(book.type=="?",type_book)
+    return render_template("free_delivery.html", title='free_delivery', books=type_book)
+
+
+
+@app.route("/kids/<string:type>")
+def kids(type):
+    if not current_user.is_authenticated:
+        return redirect(url_for('regist'))
+    type_book = book.query.filter_by(type=type).all()
+    if not type_book:
+        return redirect('/home')
+    # books = select(book).where(book.type=="?",type_book)
+    return render_template("kids.html", title='kids', books=type_book)
+
+
+
+
+
+@app.route("/checkout", methods=['GET','POST'])
+def checkout():
+    # form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    #     user_name = user(firstname=form.firstname.data, lastname=form.lastname.data, email=form.email.data, password=hashed_password)
+    #     db.session.add(user_name)
+    #     db.session.commit()
+    #     flash(f'{form.firstname.data}  تم انشاء الحساب بنجاح، من فضلك قم بتسجيل حسابك الان' , 'success')
+    #     return redirect(url_for('login'))
+    return render_template('checkout.html', title='checkout')
+
+class RegistrationForm(FlaskForm):
+    firstname = StringField('الاسم الاول', validators=[DataRequired(), Length(min=2, max=10)], render_kw={"placeholder": "الاسم الاول"})
+    lastname = StringField('اسم العائلة', validators=[DataRequired(), Length(min=2, max=10)], render_kw={"placeholder": "اسم العائلة"})
+    email = StringField('الايميل', validators=[DataRequired(), Email()], render_kw={"placeholder": "الايميل"})
+    password = PasswordField('الرقم السري', validators=[DataRequired()], render_kw={"placeholder": "الرقم السري"}) 
+    confirm_password = PasswordField('نأكيد الرقم السري', validators=[DataRequired(), EqualTo('password')], render_kw={"placeholder": "تأكيد الرقم السري"})
+     
+    submit =SubmitField('انشاء حساب')
+    
+    def validate_email(self, email):
+        user_name = user.query.filter_by(email=email.data).first()
+        if user_name:
+            raise ValidationError('هذا الحساب مسجل سابقا، يرجى تسجيل حساب اخر')
+   
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "الايميل"})
+    password = PasswordField('Password', validators=[DataRequired()], render_kw={"placeholder": "الرقم السري"}) 
+    remember = BooleanField('تذكرني')
+    submit =SubmitField('تسجيل دخول')
+    
+    
+class UpdateAccountForm(FlaskForm):
+    firstname = StringField('الاسم الاول', validators=[DataRequired(), Length(min=2, max=10)], render_kw={"placeholder": "الاسم الاول"})
+    lastname = StringField('اسم العائلة', validators=[DataRequired(), Length(min=2, max=10)], render_kw={"placeholder": "اسم العائلة"})
+    email = StringField('الايميل', validators=[DataRequired(), Email()], render_kw={"placeholder": "الايميل"})
+     
+    submit =SubmitField('تحديث الحساب')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user_name = user.query.filter_by(email=email.data).first()
+            if user_name:
+                raise ValidationError('هذا الحساب مسجل سابقا، يرجى تسجيل حساب اخر')
+
+
+
+
+
+
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user.query.get(int(user_id))
+
+class user(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    user_shop = db.relationship('shop',backref='user')
+    user_card = db.relationship('card',backref='user')
+    def __repr__(self):
+        return f"user('{self.firstname}','{self.lastname}','{self.email}')"
+    
+class book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_of_book = db.Column(db.String(50), nullable=False)
+    date_of_copyright = db.Column(db.String(50))
+    author = db.Column(db.String(50))
+    price = db.Column(db.String(50), nullable=False)
+    des_of_book = db.Column(db.Text)
+    type = db.Column(db.String(50), nullable=False)
+    image_file = db.Column(db.String(100))
+    image_file_1 = db.Column(db.String(100))
+    image_file_2 = db.Column(db.String(100))
+    image_file_3 = db.Column(db.String(100))
+    image_file_4 = db.Column(db.String(100))
+    book_shop = db.relationship('shop',backref='book')
+    book_card = db.relationship('card',backref='book')
+    def __repr__(self):
+        return f"book('{self.name_of_book}','{self.date_of_copyright}','{self.author}','{self.price}','{self.des_of_book}','{self.image_file}')"
+    
+class shop(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_of_shop = db.Column(db.String(50), nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    def __repr__(self):
+        return f"shop('{self.date_of_shop}')"
+    
+class card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    discount = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    def __repr__(self):
+        return f"card('{self.quantity}','{self.discount}')"
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+  
+    
+# if __name__ == '__main__':
+#   manager.run()
---
+++ static/main.js
@@ -0,0 +1,252 @@
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('a');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "active"
+  }
+}
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+  
+
+var swiper = new Swiper(".mySwiper_release", {
+  // Optional parameters
+  
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 5,
+  slidesPerView : 'auto',
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    
+  },
+
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+});
+
+
+
+// let carts = document.querySelectorAll('.box');
+// for (let i = 0; i < carts.length; i++){
+//   carts[i].addEventListener('click', () => {
+//     cartNumber();
+//   })
+// }
+
+// function onLoadCardNumber(){
+//   let productNumber = localStorage.getItem('cartNumber');
+
+//   if (productNumber){
+//     document.querySelector('.bag .number0fcarts').textContent = productNumber;
+//   }
+// }
+
+// function cartNumber(){
+//   let productNumber = localStorage.getItem('cartNumber');
+//   productNumber = parseInt(productNumber);
+
+//   if (productNumber){
+//     localStorage.setItem('cartNumber', productNumber + 1);
+//     document.querySelector('.bag .number0fcarts').textContent = productNumber + 1;
+//   } else{
+//     localStorage.setItem('cartNumber', 1);
+//     document.querySelector('.bag .number0fcarts').textContent = 1;
+//   }
+// }
+
+// onLoadCardNumber();
+
+
+let productInCart = JSON.parse(localStorage.getItem('ShoppingCart'));
+if(!productInCart){
+  productInCart=[];
+}
+const parentElement = document.querySelector('#buyItems');
+const cartSumPrice = document.querySelector('#sum-prices');
+const products = document.querySelectorAll('.box');
+
+
+
+const countTheSumPrice = function(){
+  let sumPrice = 0;
+  productInCart.forEach(item => {
+  sumPrice += item.price;
+  });
+  return sumPrice;
+}
+
+let checkout = []
+const updateShoppingCartHTML = function(){
+  localStorage.setItem('ShoppingCart', JSON.stringify(productInCart))
+  if (productInCart.length > 0){
+    let result = productInCart.map(product => {
+      return `
+       <li class="buyItem">
+            <img src="${product.image}">
+            <div>
+                <h5>${product.name}</h5>
+                <h6>IQD${product.price}</h6>
+                <div>
+                    <button class="button-minus" data-id=${product.id}>-</button>
+                    <span class="countOfProduct">${product.count}</span>
+                    <button class="button-plus" data-id=${product.id}>+</button>
+                </div>
+            </div>
+        </li>`
+    });
+    checkout = result
+    parentElement.innerHTML = result.join('');
+    document.querySelector('.checkout').classList.remove('hidden')
+    cartSumPrice.innerHTML= "المجوع: " + "IQD" + countTheSumPrice();
+  } else{
+    document.querySelector('.checkout').classList.add('hidden')
+    parentElement.innerHTML = '<h4 class="empty">عربة التسوق فارغة</h4>';
+    cartSumPrice.innerHTML="";
+  }
+}
+
+function updateProductInCart(product){
+  for (let i = 0; i < productInCart.length; i++){
+    if (productInCart[i].id == product.id){
+        productInCart[i].count += 1;
+        productInCart[i].price = productInCart[i].basePrice * productInCart[i].count;
+        return;
+    }
+  }
+  productInCart.push(product);
+}
+
+products.forEach(item => {
+  console.log(item);
+  item.addEventListener('click', (e) => {
+    if (e.target.classList.contains('addCart')){
+      const productID = e.target.dataset.productId;
+      const productName = item.querySelector('.productName').innerHTML;
+      const productPrice = item.querySelector('.priceValue').innerHTML;
+      const productImage = item.querySelector('img').src;
+      let product = {
+        name: productName,
+        image: productImage,
+        id: productID,
+        count: 1,
+        price: parseInt(productPrice),
+        basePrice: parseInt(productPrice)
+      }
+      updateProductInCart(product);
+      updateShoppingCartHTML();
+    }
+  });
+});
+
+
+parentElement.addEventListener('click', (e) =>{
+  const isPlusButton = e.target.classList.contains('button-plus');
+  const isMinusButton = e.target.classList.contains('button-minus');
+  if (isPlusButton || isMinusButton){
+    for (let i = 0; i < productInCart.length; i++){
+      if(productInCart[i].id === e.target.dataset.id){
+        if(isPlusButton){
+          productInCart[i].count += 1
+        }
+        else if(isMinusButton){
+          productInCart[i].count -= 1
+        }
+        productInCart[i].price = productInCart[i].basePrice * productInCart[i].count;
+      }
+      if(productInCart[i].count <= 0){
+        productInCart.splice(i, 1);
+      }
+    }
+    updateShoppingCartHTML();
+  }
+});
+
+
+updateShoppingCartHTML();
+
+const openShopCart = document.querySelector('.shoppingCartButton');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
+});
+
+function closeCart() {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
+}
+
+
+const closeShopCart = document.querySelector('#closeButton');
+const overlay = document.querySelector('.overlay_4');
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
+
+
+
+
+var swiper = new Swiper(".mySwiper_flask", {
+  
+  slidesPerView: 6,
+  spaceBetween: 4,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    
+  },
+
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+});
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+
+
